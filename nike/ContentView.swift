9c3bc580d8cd73ee28aec769c32586823ad8d218
Francesco Paciello <kekkopacio@gmail.com>
2020-12-30 23:30:47 +0100

    Initial Commit

---
+++ nike/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nike
+//
+//  Created by Kekko Paciello on 30/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nike/nikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  nikeApp.swift
+//  nike
+//
+//  Created by Kekko Paciello on 30/12/20.
+//
+
+import SwiftUI
+
+@main
+struct nikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
